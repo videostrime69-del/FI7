29e4622d0cc4f08ee7e7d96ecde59b15bc0bb370
videostrime69-del <chintanlimbachiya491@gmail.com>
2026-09-04 17:53:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// CONFIG for videostrime69-del / FI7
+const CONFIG = {
+  username: "videostrime69-del", // GitHub username
+  repo: "FI7", // Repo name - case sensitive! FI7
+  blogPostUrl: "https://urlshorterio.blogspot.com/2026/09/blog-post.html" // Change to your database post URL
+};
